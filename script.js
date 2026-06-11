45a6bcd5c8d2f77471b00125c76db2109b46407d
itzkaw <150237294+itzkaw@users.noreply.github.com>
2026-06-11 20:27:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ toggle.addEventListener("click", () => {
   }
 });
 
-
+//
 const text = "Data Analyst • Power BI • SQL • Data Storytelling";
 let i = 0;
 
@@ -47,3 +47,26 @@ function typeEffect() {
 }
 
 window.addEventListener("load", typeEffect);
+
+//
+
+const navLinks = document.querySelectorAll(".navbar a");
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
